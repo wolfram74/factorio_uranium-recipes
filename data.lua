efdb75f9f88001400c9306716bcd838b32de0f97
wolfram74 <phaugen@gmail.com>
2018-11-01 15:49:01 -0600

    included function to add all recipes to any modules that effect productivity

--- data.lua
+++ data.lua
@@ -1,2 +1,19 @@
 require("prototypes.recipes")
 require("prototypes.technology")
+
+local function productivityFix() -- courtesy of anythingapplied/Avongard
+    seen = {} -- ensures you don't fix the same unique table multiple times
+    for _, module in pairs(data.raw.module) do
+        if module.limitation and not seen[module.limitation] then
+            seen[module.limitation] = true
+            table.insert(module.limitation, "science-pack-1-plus")
+            table.insert(module.limitation, "science-pack-2-plus")
+            table.insert(module.limitation, "science-pack-3-plus")
+            table.insert(module.limitation, "military-science-pack-plus")
+            table.insert(module.limitation, "production-science-pack-plus")
+            table.insert(module.limitation, "high-tech-science-pack-plus")
+        end
+    end
+end
+
+productivityFix()
